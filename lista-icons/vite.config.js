e39bdb74a208a7c51a15c6de6cbee3c6bef65ae6
Tiago Pitombeira <tiagopitombeira1984@gmail.com>
2025-09-26 11:06:14 -0300

    Atualizando Vite Config

--- lista-icons/vite.config.js
+++ lista-icons/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/cheatsheet-icons/"
-})
+  base: "/react-icons/"  // ⚠️ deve ser exatamente o nome do seu repositório
+});
+
 
